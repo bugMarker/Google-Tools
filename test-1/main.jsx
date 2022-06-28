5d42aef949c3557e732cb449d3a5f1bde9b19033
wanghao <348977787@qq.com>
2022-06-28 22:29:21 +0800

    初始化

---
+++ test-1/main.jsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-06-28 22:22:51
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-06-28 22:22:57
+ * @FilePath: /Google-Tools/test-1/main.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+ReactDOM.render(<App />, document.getElementById("root"));
