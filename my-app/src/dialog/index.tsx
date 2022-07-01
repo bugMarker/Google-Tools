8581d6f40ccab5c8de9c33a1ebdb90c897f759ed
wanghao <348977787@qq.com>
2022-07-01 19:10:03 +0800

    init

---
+++ my-app/src/dialog/index.tsx
@@ -0,0 +1,24 @@
+/*
+ * @Author: wanghao 348977787@qq.com
+ * @Date: 2022-07-01 18:49:49
+ * @LastEditors: wanghao 348977787@qq.com
+ * @LastEditTime: 2022-07-01 18:58:29
+ * @FilePath: /my-app/src/dialog/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: red;
+`;
+
+function App() {
+  return (
+    <div>
+      <Button>你好</Button>
+    </div>
+  );
+}
+
+export default App;
